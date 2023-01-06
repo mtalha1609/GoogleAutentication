3fa3932eb9f441e2a15caaf33748756d039c0e1c
talha.asghar <talha.asghar@CS-DT-KASHIF>
2023-01-06 02:12:57 -0500

    Add project files.

---
+++ googleauthorization/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using googleauthorization.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using GoogleAuthentication.Services;
+
+namespace googleauthorization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var clientId = "164794501167-ct7gf8aioq248nh9c882iks3dp685o9u.apps.googleusercontent.com";
+            var url = "https://localhost:7235/Login/GoogleLoginCallback";
+            var response = GoogleAuth.GetAuthUrl(clientId,url);
+            ViewBag.response = response;
+            return View();
+        }
+
+  
+    }
+}
---
+++ googleauthorization/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GoogleAuthentication.Services;
+
+namespace googleauthorization.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // you will recieve the code parameter whenever user logged in by google 
+        public async Task<ActionResult> GoogleLoginCallBack(string code)
+        {
+            var clientId = "164794501167-ct7gf8aioq248nh9c882iks3dp685o9u.apps.googleusercontent.com";
+            var url = "https://localhost:7235/Login/GoogleLoginCallback";
+            var clientsecret = "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";
+
+            var token = await GoogleAuth.GetAuthAccessToken(code, clientId, clientsecret, url);
+            //getting user profile
+            var userProfile = await GoogleAuth.GetProfileResponseAsync(token.AccessToken);
+
+            ViewBag.userProfile = userProfile;
+
+            var a=_ = userProfile[0];
+            Console.WriteLine(a);
+            //userProfile.id;
+            //userProfile.email;
+            //userProfile.verified_email;
+            //userProfile.name;
+            //userProfile.given_name;
+            //userProfile.family_name;
+            //userProfile.picture;
+            //userProfile.locale;
+
+
+            return View();
+        }
+    }
+}
